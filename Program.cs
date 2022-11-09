04ccc2f2d34530485ba5f4046e36451064caf8f7
EditBySteve <stepan.vaclav.patejdl@gmail.com>
2022-11-09 17:04:24 +0100

    Added new theme

--- Program.cs
+++ Program.cs
@@ -14,11 +14,11 @@ namespace ChangeHeaderColor
 
             Console.Title = "WWHCS - Header Modifier";
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Welcome to Windows Window Header Color Selector 3.0! (WWHCS3!)");
+            Console.WriteLine("Welcome to Windows Window Header Color Selector 3.2! (WWHCS3!)");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\nWrite your desired color theme, these are the current options:");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("dark, oldschool, xbox, blood, sky, default");
+            Console.WriteLine("dark, oldschool, xbox, blood, sky, ebs, default");
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("If you want to know more about these Themes, write 'desc'");
             Console.WriteLine("If you're done, type 'quit' or 'exit'");
@@ -56,6 +56,10 @@ namespace ChangeHeaderColor
                     SetHeaderColors(5);
                     looper = 1;
                 }
+                else if(chosenTheme == "ebs") {
+                    SetHeaderColors(6);
+                    looper = 1;
+                }
                 else if(chosenTheme == "custom") {
                     Console.WriteLine("Please write Hex code already converted to Decimal");
                     Console.Write("Input Main color:");
@@ -63,12 +67,12 @@ namespace ChangeHeaderColor
                     Console.Write("Input Unfocused color:");
                     otherColor = Convert.ToInt32(Console.ReadLine());
 
-                    SetHeaderColors(6);
+                    SetHeaderColors(66);
                 }
                 else if(chosenTheme == "desc" || chosenTheme == "help")
                 {
                     Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.WriteLine("'dark' - headers are black\n'xbox' - headers are Xbox style green\n'oldschool' - dark blue color, looks bluescreen-ish\n'blood' - Bloody red\n'sky' - Sky blue, pretty\n'default' - these are just the default headers, mostly white");
+                    Console.WriteLine("'dark' - headers are black\n'xbox' - headers are Xbox style green\n'oldschool' - dark blue color, looks bluescreen-ish\n'blood' - Bloody red\n'sky' - Sky blue, pretty\n'ebs' - My favourite, red and blue\n'default' - these are just the default headers, mostly white");
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Now, choose one of the Themes mentioned above:");
                 }
@@ -78,9 +82,9 @@ namespace ChangeHeaderColor
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid Theme.");
+                    Console.WriteLine("Invalid command.");
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("\nWrite a VALID Theme, from the options above:");
+                    Console.WriteLine("\nWrite a VALID command, from the options above:");
                     
                 }
             }
@@ -136,7 +140,14 @@ namespace ChangeHeaderColor
                 color[2] = 0x00ffd799;
                 color[3] = 1;
             }
-            if(selectedTheme == 6) 
+            if(selectedTheme == 6) //Red and Blue
+            {
+                color[0] = 0x00b44900;
+                color[1] = 0x002003b;
+                color[2] = 0x00b44900;
+                color[3] = 1;
+            }
+            if(selectedTheme == 66) 
             {
                 color[0] = mainColor;
                 color[1] = otherColor;
